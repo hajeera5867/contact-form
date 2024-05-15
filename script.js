d034412e2618f2726e5af61b4f5db70a28b4e34e
hajeera5867 <bibihajeera5867@gmail.com>
2024-05-15 15:15:41 +0530

    hii

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateage(){
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var result = document.querySelector("p");
+
+    if(age > 18 ){
+        result.innerHTML = "you are eligible to vote😊";
+    }else{
+        result.innerHTML = "you are still child 🤣😂"
+    }
+
+}
